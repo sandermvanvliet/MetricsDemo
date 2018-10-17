7c4b958d825b8bfa63c4ea02862bddada3317c78
Sander van Vliet <sander@codenizer.nl>
2018-10-17 10:48:42 +0200

    Use timer rather than Thread.Sleep

--- src/Metrics.Demo.Cli/MetricPusherService.cs
+++ src/Metrics.Demo.Cli/MetricPusherService.cs
@@ -10,53 +10,46 @@ namespace Metrics.Demo.Cli
     public class MetricPusherService : IHostedService
     {
         private readonly ILogger logger;
-        private readonly CancellationTokenSource _tokenSource;
-        private Task _pusherTask;
+        private Timer _timer;
 
         public MetricPusherService(ILogger logger)
         {
             this.logger = logger;
-            _tokenSource = new CancellationTokenSource();
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
             logger.Information("Starting metric pusher");
 
-            _pusherTask = Task.Factory.StartNew(() => PushMetrics());
-
-            return Task.CompletedTask;
-        }
-
-        private void PushMetrics()
-        {
             var random = new Random();
+            var delay = random.Next(250, 1000);
 
-            while (!_tokenSource.Token.IsCancellationRequested)
-            {
-                Thread.Sleep(1000);
-
-                var loopValue = random.Next(10, 100);
-                var tagValue = random.Next(1, 100) % 2 == 0 ? "red" : "blue";
-
-                logger.Information("Pushing metric with loop value {loop_value}", loopValue);
-                
-                DatadogStats.Default.Increment("number_of_loops");
-                DatadogStats.Default.Gauge("loop_value", loopValue, tags: new[] {"type:" + tagValue });
-            }
-
-            logger.Warning("Cancellation requested");
+            _timer = new Timer(GenerateMetric, null, TimeSpan.FromMilliseconds(delay), TimeSpan.FromSeconds(1));
+            
+            return Task.CompletedTask;
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
             logger.Information("Stopping metric pusher");
 
-            _tokenSource.Cancel();
-
-            _pusherTask.Wait();
+            _timer.Change(Timeout.InfiniteTimeSpan, TimeSpan.Zero);
+            _timer.Dispose();
+            _timer = null;
 
             return Task.CompletedTask;
+        }
+
+        private void GenerateMetric(object state)
+        {
+            var random = new Random();
+            var loopValue = random.Next(10, 100);
+            var tagValue = random.Next(1, 100) % 2 == 0 ? "red" : "blue";
+
+            logger.Information("Pushing metric with loop value {loop_value}", loopValue);
+            
+            DatadogStats.Default.Increment("number_of_loops");
+            DatadogStats.Default.Gauge("loop_value", loopValue, tags: new[] {"type:" + tagValue });
         }
     }
 }
